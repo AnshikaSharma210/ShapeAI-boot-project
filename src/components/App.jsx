007474a6dabd2714257314ef95129f3d843b02a5
Anshika Sharma <83543266+AnshikaSharma210@users.noreply.github.com>
2021-08-04 20:59:54 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Boot from "./Boot";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Boot />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>React JS Bootcamp</h1>
+      <p>Last day of bootcamp.It was fun learning everything.</p>
+    </div>
+  );
+}
+
+export default Boot;
